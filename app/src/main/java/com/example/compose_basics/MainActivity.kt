8568cff7f216e2909364eb023d61f44a40d35b48
Cephas Zulu <48860145+cazterk@users.noreply.github.com>
2022-12-27 17:53:16 +0200

    using @Composable functions

--- app/src/main/java/com/example/compose_basics/MainActivity.kt
+++ app/src/main/java/com/example/compose_basics/MainActivity.kt
@@ -1,19 +1,27 @@
 package com.example.compose_basics
 
+import android.accounts.AuthenticatorDescription
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.AndroidPaint
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.BitmapPainter
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.compose_basics.ui.theme.ComposebasicsTheme
@@ -22,24 +30,37 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column(
-                modifier = Modifier
-                    .background(Color.Green)
-                    .fillMaxHeight(0.5f)
-                    .fillMaxWidth()
-                    .border(5.dp, Color.Magenta)
-                    .padding(5.dp)
-                    .border(5.dp, Color.Blue)
-                    .padding(5.dp)
-                    .border(10.dp, Color.Red)
-                    .padding(10.dp)
-            )
-            {
+                   {
 
-                Text("World")
-                Spacer(modifier = Modifier.height(50.dp))
-                Text("Hello")
+
             }
+        }
+    }
+}
+
+@Composable
+fun ImageCard(
+    painter: Painter,
+    contentDescription: String,
+    title: String,
+    modifier: Modifier = Modifier
+
+){
+    Card(
+        modifier = modifier.fillMaxHeight(),
+        shape = RoundedCornerShape(15.dp),
+        elevation =5.dp
+    ){
+        Box(modifier = Modifier.height(200.dp) ){
+            Image(painter,
+            contentDescription = contentDescription,
+            contentScale = ContentScale.Crop
+
+            )
+        Box(modifier = Modifier.fillMaxSize().padding(12.dp),
+        ){
+            Text("")
+        }
         }
     }
 }
