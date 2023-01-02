63110cacadf876f821e46e525d2d539e9b4a7c3c
Cephas Zulu <48860145+cazterk@users.noreply.github.com>
2023-01-02 17:18:33 +0200

    ConstraintLayout

--- app/src/main/java/com/example/compose_basics/MainActivity.kt
+++ app/src/main/java/com/example/compose_basics/MainActivity.kt
@@ -17,6 +17,7 @@ import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
@@ -30,6 +31,10 @@ import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ChainStyle
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.ConstraintSet
+import androidx.constraintlayout.compose.Dimension
 import kotlinx.coroutines.launch
 import kotlin.random.Random
 import androidx.compose.runtime.mutableStateOf as mutableStateOf
@@ -54,7 +59,8 @@ class MainActivity : ComponentActivity() {
 //            BasicState()
 //            StyledText()
 //            Controls()
-            ListLayouts()
+//            ListLayouts()
+              ConstraintLayouts()
 
 
         }
@@ -265,5 +271,43 @@ fun ListLayouts(){
                     .padding(vertical = 24.dp)
             )
         }
+    }
+}
+
+// ConstraintLayout
+
+@Composable
+fun ConstraintLayouts() {
+    val constraints = ConstraintSet {
+        val greenBox = createRefFor("greenbox")
+        val redBox = createRefFor("redbox")
+        val guideline = createGuidelineFromTop(0.5f)
+
+        constrain(greenBox) {
+            top.linkTo(guideline)
+            start.linkTo(parent.start)
+            width = Dimension.value(100.dp)
+            height = Dimension.value(100.dp)
+        }
+
+        constrain(redBox) {
+            top.linkTo(parent.top)
+            start.linkTo(greenBox.end)
+            end.linkTo(parent.end)
+            width = Dimension.value(100.dp)
+            height = Dimension.value(100.dp)
+        }
+        createHorizontalChain(greenBox, redBox, chainStyle = ChainStyle.Packed)
+    }
+    ConstraintLayout(
+        constraints,
+        modifier = Modifier
+            .fillMaxSize()) {
+        Box(modifier = Modifier
+            .background(Color.Green)
+            .layoutId("greenbox"))
+        Box(modifier = Modifier
+            .background(Color.Red)
+            .layoutId("redbox"))
     }
 }
