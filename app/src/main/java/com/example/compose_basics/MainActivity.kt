7c1be6e87c7ae7b09c6b49f9b48b654e4173a6e0
Cephas Zulu <48860145+cazterk@users.noreply.github.com>
2023-01-05 12:12:03 +0200

    simple animations

--- app/src/main/java/com/example/compose_basics/MainActivity.kt
+++ app/src/main/java/com/example/compose_basics/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.compose_basics
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColor
+import androidx.compose.animation.core.*
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
@@ -35,6 +37,7 @@ import androidx.constraintlayout.compose.ChainStyle
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.ConstraintSet
 import androidx.constraintlayout.compose.Dimension
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlin.random.Random
 import androidx.compose.runtime.mutableStateOf as mutableStateOf
@@ -60,7 +63,8 @@ class MainActivity : ComponentActivity() {
 //            StyledText()
 //            Controls()
 //            ListLayouts()
-              ConstraintLayouts()
+//              ConstraintLayouts()
+            SimpleAnimations()
 
 
         }
@@ -311,3 +315,59 @@ fun ConstraintLayouts() {
             .layoutId("redbox"))
     }
 }
+
+// simple animations
+
+@Composable
+fun SimpleAnimations(){
+    var sizeState by remember {
+        mutableStateOf(200.dp)
+    }
+    val size by animateDpAsState(
+        targetValue = sizeState,
+//        tween(
+//            durationMillis = 3000,
+//            delayMillis =300,
+//            easing = LinearOutSlowInEasing
+//        )
+//        spring(
+//            Spring.DampingRatioHighBouncy
+//        )
+
+//          keyframes {
+//              durationMillis= 5000
+//              sizeState at 0 with LinearEasing
+//              sizeState * 1.5f at 1000 with FastOutLinearInEasing
+//              sizeState * 2f at 5000
+//          }
+            tween(
+                durationMillis = 1000,
+            )
+    )
+    val infiniteTransition = rememberInfiniteTransition()
+    val color by infiniteTransition.animateColor(
+        initialValue = Color.Red,
+        targetValue = Color.Green,
+        animationSpec = infiniteRepeatable(
+            tween(durationMillis = 2000),
+            repeatMode = RepeatMode.Reverse
+        )
+    )
+    Box(
+        modifier = Modifier
+            .size(size)
+            .background(color),
+        contentAlignment = Alignment.Center
+    ){
+    Button(onClick = {
+        sizeState += 50.dp
+    }) {
+        Text("Increase Size")
+    }
+
+    }
+
+
+}
+
+
